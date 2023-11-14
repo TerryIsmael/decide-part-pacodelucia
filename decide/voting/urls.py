81e3a94310a48fdfd7566dccb2850e377cf5f83b
rafgoncas1 <rafaelgonzalezcastillero@gmail.com>
2023-11-14 13:48:55 +0100

    feat : create backend endpoint get voting str

--- decide/voting/urls.py
+++ decide/voting/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from .views import getVoteStringKeys
 
 
 urlpatterns = [
     path('', views.VotingView.as_view(), name='voting'),
     path('<int:voting_id>/', views.VotingUpdate.as_view(), name='voting'),
+    path('<int:voting_id>/stringkeys', getVoteStringKeys, name='voting'),
 ]
