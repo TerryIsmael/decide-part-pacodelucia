0a6cea0f118b5f5aac5fd3b29ed07eb45d927abf
ismbargar <ismbargar@alum.us.es>
2023-11-14 21:12:25 +0100

    feat: Añadido edición, borrado y cambio estado #17
    Co-authored-by: marmarsol4 <marmarsol4@users.noreply.github.com>

--- decide-frontend/src/components/Voting.vue
+++ decide-frontend/src/components/Voting.vue
@@ -2,7 +2,7 @@
 import { ref, onMounted } from "vue";
 import Voting from "../../models/Voting.js";
 
-//TODO: Cambiar estado de votacion
+// TODO: redirigir añadir questions y auths redirigiendo
 
 export default {
   setup() {
@@ -61,16 +61,60 @@ export default {
       }
     };
 
-    const saveVoting = async (votacion) => {
+    const applyAction = async (votingId, action) => {
+      const votingAccion = {
+        id: votingId,
+        action: action,
+      };
+      try {
+        const response = await fetch("http://localhost:8000/voting/voting/",
+          {
+            method: "PUT",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            credentials: "include",
+            body: JSON.stringify(votingAccion),
+          }
+        );
+      } catch (error) {
+        console.error("Error:", response.status, error.json());
+      }
+      fetchvotings();
+    };
+    
+    const deleteVoting = async (id) =>{
+      const votingDelete = {
+        id: id,
+      };
+      try {
+        const response = await fetch("http://localhost:8000/voting/voting/",
+          {
+            method: "DELETE",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            credentials: "include",
+            body: JSON.stringify(votingDelete),
+          }
+        );
+      } catch (error) {
+        console.error("Error:", response.status, error.json());
+      }
+      fetchvotings();
+    }
+
+    const saveVoting = async (voting) => {
       editing.value = false;
 
-      const votacionPost = {
-        name: votacion.name,
-        desc: votacion.desc,
-        question: votacion.question.id,
-        auths: votacion.auths.map((auth) => auth.id),
+      const votingPost = {
+        id: voting.id,
+        name: voting.name,
+        desc: voting.desc,
+        question: voting.question.id,
+        auths: voting.auths.map((auth) => auth.id),
       };
-      console.log(votacionPost)
+      
       try {
         const response = await fetch("http://localhost:8000/voting/voting/", {
           method: "POST",
@@ -78,7 +122,7 @@ export default {
             "Content-Type": "application/json",
           },
           credentials: "include",
-          body: JSON.stringify(votacionPost),
+          body: JSON.stringify(votingPost),
         });
       } catch (error) {
         console.error("Error:", response.status, error.json());
@@ -88,12 +132,12 @@ export default {
       fetchvotings();
     };
 
-    const isNumberInTally = (votacion, number) => {
-      return votacion.tally.includes(number);
+    const isNumberInTally = (voting, number) => {
+      return voting.tally.includes(number);
     };
 
-    const showForm = (votacion) => {
-      selectedVoting.value = votacion;
+    const showForm = (voting) => {
+      selectedVoting.value = voting;
     };
 
     const changeSelected = (id) => {
@@ -136,6 +180,8 @@ export default {
       changeEditing,
       newVoting,
       saveVoting,
+      applyAction,
+      deleteVoting,
     };
   },
 };
@@ -145,12 +191,7 @@ export default {
   <div>
     <h2>Listado de votaciones</h2>
     <ul>
-      <button
-        class="little-button"
-        @click="changeSelected(New), changeEditing(true)"
-      >
-        Nueva votación
-      </button>
+      <button class="little-button" @click="changeSelected(New), changeEditing(true)"> Nueva votación </button>
 
       <div v-if="selectedVoting == 'New' && editing == true">
         <form @submit.prevent="saveVoting(newVoting)">
@@ -165,69 +206,58 @@ export default {
           <div>
             <label for="question">Pregunta: </label>
             <select required id="question" v-model="newVoting.question">
-              <option
-                v-for="question in questions"
-                :key="question.id"
-                :value="question"
-              >
-                {{ question.desc }}
-              </option>
+              <option v-for="question in questions" :key="question.id" :value="question"> {{ question.desc }} </option>
             </select>
           </div>
           <div>
             <label for="auths">Auths: </label>
             <select required id="auths" v-model="newVoting.auths" multiple>
-              <option v-for="auth in auths" :key="auth.id" :value="auth">
-                {{ auth.name }}
-              </option>
+              <option v-for="auth in auths" :key="auth.id" :value="auth"> {{ auth.name }} </option>
             </select>
           </div>
           <div>
-            <button class="little-button" @click="changeEditing(false)">
-              Cancelar
-            </button>
+            <button class="little-button" @click="changeEditing(false)"> Cancelar </button>
             <button type="submit" class="little-button">Crear</button>
           </div>
         </form>
       </div>
 
-      <li v-for="votacion in votings" :key="votacion.id">
+      <li v-for="voting in votings" :key="voting.id">
         <h3>
-          <button
-            @click="
-              changeSelected(votacion.id);
-              changeEditing(false);
-            "
-          >
-            {{ votacion.name }}
-            <span v-if="votacion.start_date != null"> - </span>
-            {{ dateFormat(votacion.start_date) }}
-            <span v-if="votacion.end_date != null"> - </span>
-            {{ dateFormat(votacion.end_date) }}
+          <button 
+          @click="changeSelected(voting.id);changeEditing(false);">
+            {{ voting.name }}
+            <span v-if="voting.start_date != null"> - </span>
+            {{ dateFormat(voting.start_date) }}
+            <span v-if="voting.end_date != null"> - </span>
+            {{ dateFormat(voting.end_date) }}
           </button>
         </h3>
 
-        <div v-if="selectedVoting === votacion.id">
-          <p><span class="bold">Id:</span> {{ votacion.id }}</p>
+        <div v-if="selectedVoting === voting.id">
+
+          <button v-if="voting.start_date == null " class="little-button" @click="applyAction(voting.id, 'start')"> Empezar </button>
+          <button v-if="voting.start_date != null && voting.end_date == null" class="little-button" @click="applyAction(voting.id, 'stop')"> Parar </button>
+          <button v-if="voting.end_date != null && voting.postproc == null" class="little-button" @click="applyAction(voting.id, 'tally')"> Recuento </button>
+
+          <p><span class="bold">Id:</span> {{ voting.id }}</p>
           <div v-if="!editing">
-            <p><span class="bold">Descripción: </span>{{ votacion.desc }}</p>
+            <p><span class="bold">Descripción: </span>{{ voting.desc }}</p>
             <p>
-              <span class="bold">Pregunta: </span> {{ votacion.question.desc }}
+              <span class="bold">Pregunta: </span> {{ voting.question.desc }}
             </p>
             <p>
               <span class="bold">Opción ganadora: </span>
-              {{
-                votacion.tally ? votacion.tally.toString() : "No hay recuento"
-              }}
+              {{ voting.tally ? voting.tally.toString() : "No hay recuento" }}
             </p>
 
-            <table v-if="votacion.postproc == null">
+            <table v-if="voting.postproc == null">
               <tr>
                 <th>Número opción</th>
                 <th>Opción</th>
               </tr>
 
-              <tr v-for="option in votacion.question.options">
+              <tr v-for="option in voting.question.options">
                 <td>{{ option.number }}</td>
                 <td>{{ option.option }}</td>
               </tr>
@@ -240,13 +270,7 @@ export default {
                 <th>Votos</th>
               </tr>
               <tbody>
-                <tr
-                  v-for="option in votacion.postproc"
-                  :key="option.number"
-                  :class="{
-                    remarkable: isNumberInTally(votacion, option.number),
-                  }"
-                >
+                <tr v-for="option in voting.postproc" :key="option.number" :class="{remarkable: isNumberInTally(voting, option.number),}">
                   <td>{{ option.number }}</td>
                   <td>{{ option.option }}</td>
                   <td>{{ option.votes }}</td>
@@ -260,7 +284,7 @@ export default {
                 <th>URL</th>
                 <th>Yo</th>
               </tr>
-              <tr v-for="auth in votacion.auths">
+              <tr v-for="auth in voting.auths">
                 <td>{{ auth.name }}</td>
                 <td>{{ auth.url }}</td>
                 <td>{{ auth.me }}</td>
@@ -271,32 +295,23 @@ export default {
           <div v-else>
             <div>
               <label for="name">Nombre: </label>
-              <input type="text" id="name" v-model="votacion.name" />
+              <input type="text" id="name" v-model="voting.name" />
             </div>
             <div>
               <label for="desc">Descripción: </label>
-              <textarea
-                id="desc"
-                rows="10"
-                columns="90"
-                v-model="votacion.desc"
-              ></textarea>
+              <textarea id="desc" rows="10" columns="90" v-model="voting.desc"></textarea>
             </div>
             <div>
               <label for="question">Pregunta: </label>
-              <select id="question" v-model="votacion.question">
-                <option
-                  v-for="question in questions"
-                  :key="question.id"
-                  :value="question"
-                >
+              <select id="question" v-model="voting.question">
+                <option v-for="question in questions" :key="question.id" :value="question">
                   {{ question.desc }}
                 </option>
               </select>
             </div>
             <div>
               <label for="auths">Auths: </label>
-              <select id="auths" v-model="votacion.auths" multiple>
+              <select id="auths" v-model="voting.auths" multiple>
                 <option v-for="auth in auths" :key="auth.id" :value="auth">
                   {{ auth.name }}
                 </option>
@@ -304,34 +319,29 @@ export default {
             </div>
           </div>
 
-          <p v-if="votacion.start_date != null">
+          <p v-if="voting.start_date != null">
             <span class="bold">Fecha de inicio: </span>
-            {{ dateFormat(votacion.start_date) }}
+            {{ dateFormat(voting.start_date) }}
           </p>
-          <p v-if="votacion.end_date != null">
+          <p v-if="voting.end_date != null">
             <span class="bold">Fecha de fin: </span>
-            {{ dateFormat(votacion.end_date) }}
+            {{ dateFormat(voting.end_date) }}
           </p>
-          <h4 v-if="votacion.postproc == null">No finalizada</h4>
-          <div v-if="votacion.start_date != null">
+          <h4 v-if="voting.postproc == null">No finalizada</h4>
+          <div v-if="voting.start_date != null">
             <h4>- PUB_KEY -</h4>
-            <p><span class="bold">P:</span> {{ BigInt(votacion.pub_key.p) }}</p>
-            <p><span class="bold">G:</span> {{ BigInt(votacion.pub_key.g) }}</p>
-            <p><span class="bold">Y:</span> {{ BigInt(votacion.pub_key.y) }}</p>
+            <p><span class="bold">P:</span> {{ BigInt(voting.pub_key.p) }}</p>
+            <p><span class="bold">G:</span> {{ BigInt(voting.pub_key.g) }}</p>
+            <p><span class="bold">Y:</span> {{ BigInt(voting.pub_key.y) }}</p>
           </div>
 
           <div v-if="!editing">
-            <button class="little-button" @click="changeEditing(true)">
-              Editar
-            </button>
+            <button v-if="voting.start_date == null" class="little-button" @click="changeEditing(true)"> Editar </button>
+            <button class="little-button deleteButton" @click="deleteVoting(voting.id)"> Eliminar </button>
           </div>
           <div v-else>
-            <button class="little-button" @click="changeEditing(false)">
-              Cancelar
-            </button>
-            <button class="little-button" @click="saveVoting(votacion)">
-              Guardar
-            </button>
+            <button class="little-button" @click="changeEditing(false)"> Cancelar </button>
+            <button class="little-button" @click="saveVoting(voting)"> Guardar </button>
           </div>
         </div>
       </li>
@@ -349,6 +359,10 @@ export default {
   border-radius: 5px;
   border: 2px solid grey;
   width: 67%;
+}
+
+.deleteButton {
+  background-color: rgb(211, 91, 91);
 }
 
 </style>
--- decide/voting/views.py
+++ decide/voting/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework import generics, status, permissions
 from rest_framework.response import Response
 
+from store.models import Vote 
 from .models import Question, QuestionOption, Voting
 from .serializers import SimpleVotingSerializer, VotingSerializer, QuestionSerializer
 from base.serializers import AuthSerializer
@@ -122,6 +123,7 @@ class VotingFrontView(generics.ListCreateAPIView):
     filterset_fields = ('id', )
 
     def get(self, request, *args, **kwargs):
+        
         idpath = kwargs.get('voting_id')
         self.queryset = Voting.objects.all()
         version = request.version
@@ -133,6 +135,7 @@ class VotingFrontView(generics.ListCreateAPIView):
         return super().get(request, *args, **kwargs)
 
     def post(self, request, *args, **kwargs):
+
         self.permission_classes = (UserIsStaffOrAdmin,)
         self.check_permissions(request)
         for data in ['name', 'desc', 'question', 'auths']:
@@ -140,9 +143,14 @@ class VotingFrontView(generics.ListCreateAPIView):
                 return Response({}, status=status.HTTP_400_BAD_REQUEST)
             
         question = Question.objects.get(id=request.data.get('question'))
-        voting = Voting(name=request.data.get('name'), desc=request.data.get('desc'),
-                question=question)
-        
+        if not request.data.get("id"):
+            voting = Voting(name=request.data.get('name'), desc=request.data.get('desc'),
+                    question=question)
+        else:
+            voting = Voting.objects.get(id=request.data.get('id'))
+            voting.name = request.data.get('name')
+            voting.desc = request.data.get('desc')
+            voting.question = question
         voting.save()
         authsIds = request.data.get('auths')
         auths = Auth.objects.filter(id__in=authsIds)
@@ -150,3 +158,74 @@ class VotingFrontView(generics.ListCreateAPIView):
         voting.auths.set(auths)
 
         return Response({}, status=status.HTTP_201_CREATED)
+    
+    def put(self, request, *args, **kwars):
+
+        action = request.data.get('action')
+        if not action:
+            return Response({}, status=status.HTTP_400_BAD_REQUEST)
+
+        voting = get_object_or_404(Voting, pk=request.data.get('id'))
+        msg = ''
+        st = status.HTTP_200_OK
+
+        if action == 'start':
+            if voting.start_date:
+                msg = 'Voting already started'
+                st = status.HTTP_400_BAD_REQUEST
+            else:
+                voting.create_pubkey()
+                voting.start_date = timezone.now()
+                voting.save()
+                msg = 'Voting started'
+
+        elif action == 'stop':
+            if not voting.start_date:
+                msg = 'Voting is not started'
+                st = status.HTTP_400_BAD_REQUEST
+            elif voting.end_date:
+                msg = 'Voting already stopped'
+                st = status.HTTP_400_BAD_REQUEST
+            else:
+                voting.end_date = timezone.now()
+                voting.save()
+                msg = 'Voting stopped'
+        
+        elif action == 'tally':
+            if not voting.start_date:
+                msg = 'Voting is not started'
+                st = status.HTTP_400_BAD_REQUEST
+            elif not voting.end_date:
+                msg = 'Voting is not stopped'
+                st = status.HTTP_400_BAD_REQUEST
+            elif voting.tally:
+                msg = 'Voting already tallied'
+                st = status.HTTP_400_BAD_REQUEST
+            else:   
+                token = request.session.get('auth-token', '')
+                if Vote.objects.filter(voting_id=voting.id).exists():
+                    voting.tally_votes(token)
+                    msg = 'Voting tallied'
+                else:
+                    voting.postproc = []
+                    for option in voting.question.options.all():
+                        votes = 0
+                        voting.postproc.append({
+                            'option': option.option,
+                            'number': option.number,
+                            'votes': votes,
+                            'postproc': 0
+                        })
+                    voting.tally = []
+                    voting.save()
+                    msg = 'No votes to tally'
+        else:
+            msg = 'Action not found, try with start, stop or tally'
+            st = status.HTTP_400_BAD_REQUEST
+        return Response(msg, status=st)
+    
+    def delete(self, request, *args, **kwars):
+        voting = get_object_or_404(Voting, pk=request.data.get('id'))
+        voting.delete()
+        return Response({}, status=status.HTTP_200_OK)
+    
