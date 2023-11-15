51d33790952192a3c815bb01cbfa0ee3ee6e3226
darkgigi <daniel.arriaza.arriaza@gmail.com>
2023-11-15 14:12:15 +0100

    feat: Creadas pantallas para login de admin #1

--- decide-frontend/src/router.js
+++ decide-frontend/src/router.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HelloWorld from './components/HelloWorld.vue';
 import Tokens from './components/Tokens.vue';
+import AdminLogin from './components/AdminLogin.vue';
 
 const routes = [
   { path: '/admin/authtoken', component: Tokens },
-  { path: '/', component: HelloWorld }
+  { path: '/', component: HelloWorld },
+  { path: '/admin/login', component: AdminLogin },
 ];
 
 const router = createRouter({
