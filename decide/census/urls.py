87fdfea9d3181f18f728bc4b3b2ac270597d9e6e
miguel-angel-roldan-garcia <migrolgar2@alum.us.es>
2023-11-16 00:24:10 +0100

    feat: Implementada exportación censo #44
    El censo es ahora exportable a un archivo xlsx.

--- decide/census/urls.py
+++ decide/census/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('', views.CensusCreate.as_view(), name='census_create'),
     path('<int:voting_id>/', views.CensusDetail.as_view(), name='census_detail'),
     path('import/', views.CensusImport.as_view(), name='census import'),
-    path('import/ldap/', views.CensusImportLDAP.as_view(), name='census import ldap')
+    path('import/ldap/', views.CensusImportLDAP.as_view(), name='census import ldap'),
+    path('export/<int:voting_id>', views.CensusExport.as_view(), name="census export"),
+    path('export/', views.CensusExport.as_view(), name="census export")
 ]
