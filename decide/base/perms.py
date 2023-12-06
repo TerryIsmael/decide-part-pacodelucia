ede04a8f20edc15d572bbe5df1c6d627b66fac27
ismbargar <ismbargar@alum.us.es>
2023-12-06 20:42:17 +0100

    fix: Solucionado problema de permisos #91
    Closes #91

--- decide/base/perms.py
+++ decide/base/perms.py
@@ -16,7 +16,9 @@ class UserIsStaffOrAdmin(permissions.BasePermission):
 
     def has_permission(self, request, view):
         if not request.auth:
-            return [permissions.IsAdminUser]
+            if request.user.is_anonymous:
+                return False
+            return request.user.is_staff or request.user.is_superuser
         response = mods.post('authentication/getuser', json={'token': request.auth.key},
                 response=True)
         return response.json().get('is_staff', False) or response.json().get('is_admin', False)
