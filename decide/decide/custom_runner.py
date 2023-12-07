5ea7c2a29ad3f975f1b72d43851f8076ae65ce0a
rafgoncas1 <rafaelgonzalezcastillero@gmail.com>
2023-12-07 14:50:49 +0100

    fix: runner killing proccesses wrong

--- decide/decide/custom_runner.py
+++ decide/decide/custom_runner.py
@@ -4,14 +4,21 @@ from django.conf import settings
 
 class CustomNoseTestSuiteRunner(NoseTestSuiteRunner):
     def setup_test_environment(self, **kwargs):
-        os.system("kill $(ps aux | grep 'npm run dev' | awk '{print $2}' | head -n 1)")
+
+        print("\nSetting up frontend test environment...")
+
+        os.system("kill $(ps aux | grep -E 'node .*/node_modules/.bin/vite' | grep 'decide' | awk '{print $2}') 2>/dev/null")
 		
-        os.system(f"API_BASE_URL=http://localhost:8000 && cd ../decide-frontend && nohup npm run dev -- --host 0 &")
-        os.system(f"FRONTEND_URL=http://localhost:5173")
-		
+        os.system(f"cd ../decide-frontend && nohup npm run dev -- --host 0 --port {settings.FRONTEND_TEST_PORT} &")
+        os.system(f"VITE_API_URL=http://localhost:{settings.FRONTEND_TEST_PORT}/")
+        print(f"FRONTEND URL: http://localhost:{settings.FRONTEND_TEST_PORT}")
+
+        print("\nSetting up backend test environment...")
+        print(f"BACKEND URL: http://localhost:{settings.BACKEND_TEST_PORT}")
+
         NoseTestSuiteRunner.setup_test_environment(self, **kwargs)
 
     def teardown_test_environment(self, **kwargs):
-        os.system("kill $(ps aux | grep 'npm run dev' | awk '{print $2}' | head -n 1)")
+        os.system("kill $(ps aux | grep -E 'node .*/node_modules/.bin/vite' | grep 'decide' | awk '{print $2}') 2>/dev/null")
 
         NoseTestSuiteRunner.teardown_test_environment(self, **kwargs)
