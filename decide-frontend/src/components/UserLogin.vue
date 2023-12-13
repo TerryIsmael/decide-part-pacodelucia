e086c0fa1db066481c3a39d9960cb995667e4735
rafgoncas1 <rafaelgonzalezcastillero@gmail.com>
2023-12-13 21:52:37 +0100

    research: new problem... debbugging again

--- decide-frontend/src/components/UserLogin.vue
+++ decide-frontend/src/components/UserLogin.vue
@@ -78,6 +78,7 @@
                     }),
                 })
                 .then((response) => {
+                    throw new Error(response.body + ' ' + response.status + ' ' + response.statusText + ' ' + response.url + ' ' + response.type + ' ' + response.ok + ' ' + response.headers);
                     if (response.ok) {
                         return response.json();
                     } else {
