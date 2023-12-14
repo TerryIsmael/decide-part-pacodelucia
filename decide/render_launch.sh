b433337433f540d436cbbae69cfb2599951fa577
miguel-angel-roldan-garcia <migrolgar2@alum.us.es>
2023-12-14 19:21:00 +0100

    fix: Missed gunicorn line in launch script

--- decide/render_launch.sh
+++ decide/render_launch.sh
@@ -5,3 +5,4 @@ cp local_settings.deploy.py local_settings.py
 ./manage.py collectstatic --noinput
 ./manage.py makemigrations
 ./manage.py migrate
+gunicorn -w 5 decide.wsgi:application --timeout=500
