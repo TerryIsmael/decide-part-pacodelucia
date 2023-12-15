ff6f3f80a58ea0e1231bb0359fcb1dac2de5007e
miguel-angel-roldan-garcia <migrolgar2@alum.us.es>
2023-12-15 12:55:42 +0100

    fix: Undo settings installed apps order change

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -31,8 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'corsheaders',
-
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -40,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'corsheaders',
     'django_filters',
     'rest_framework',
     'rest_framework.authtoken',
