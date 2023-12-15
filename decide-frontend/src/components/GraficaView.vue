27541ca237e4d801214172105c83c8f195417d2a
Yuuta094 <YvanSan094@gmail.com>
2023-12-15 04:33:35 +0100

    Feat: Añadida tabla de questions por cada votación

--- decide-frontend/src/components/GraficaView.vue
+++ decide-frontend/src/components/GraficaView.vue
@@ -1,74 +1,136 @@
 <template>
     <div>
+      <div>
+        <button v-for="(voting, index) in votings" :key="index" @click="showDetails(voting)">
+          {{ voting.name }}
+        </button>
+      </div>
+      <div>
         <canvas ref="myChart" style="width: 100%; height: 400px;"></canvas>
+      </div>
+      <div v-if="selectedVoting">
+        <h2>{{ selectedVoting.name }} Details</h2>
+        <ul>
+          <li v-for="(tally, questionIndex) in selectedVoting.tally" :key="questionIndex">
+            {{ getOptionName(questionIndex) }}: {{ [tally].length || 0 }} votes
+          </li>
+        </ul>
+        <!-- Nueva sección para la gráfica de detalles -->
+        <div>
+            <canvas ref="detailChartCanvas" style="width: 50%; height: 300px;"></canvas>        
+        </div>
+      </div>
     </div>
-</template>
+  </template>
+ <script>
+ import Chart from 'chart.js/auto';
+ import axios from 'axios';
+ 
+ export default {
+   data() {
+     return {
+       votings: [],
+       selectedVoting: null,
+       colors: generateRandomColors(10),
+       detailChart: null,
+     };
+   },
+   mounted() {
+     this.fetchData();
+   },
+   methods: {
 
-<script>
-import Chart from 'chart.js/auto';
-import axios from 'axios';
-export default {
-    data() {
-        return {
-            labels: [],
-            data: [],
-            colors: generateGreenScale(10), // Cambia el número según el nº de votaciones
-        };
+    getOptionName(optionIndex) {
+    return this.selectedVoting.question.options[optionIndex];
     },
-    mounted() {
-        this.fetchData();
-    },
-    methods: {
-        fetchData() {
-            axios.get('http://localhost:8000/voting/')
-                .then(response => {
-                    this.processData(response.data);
-                    this.createChart();
-                })
-                .catch(error => {
-                    console.error('Error fetching data:', error);
-                });
-        },
-        processData(data) {
-            this.labels = data.map((voting, index) => `${voting.name}: ${voting.tally.length || 0} votes`);
-            this.data = data.map(voting => voting.tally.length || 0);
-            
-        },
-        createChart() {
-            const ctx = this.$refs.myChart.getContext('2d');
-            new Chart(ctx, {
-                type: 'bar',
-                data: {
-                    labels: this.labels,
-                    datasets: [{
-                        label: 'Total Votes',
-                        data: this.data,
-                        backgroundColor: this.colors,
-                        borderWidth: 1,
-                    }],
-                },
-                options: {
-                    scales: {
-                        y: {
-                            beginAtZero: true,
-                        },
-                    },
-                },
-            });
-        },
-    },
-};
-function generateGreenScale(numColors) {
-    const colors = [];
-    for (let i = 1; i < numColors; i++) {
-        const greenValue = Math.round((255 / numColors) * i);
-        const color = `rgba(1, ${greenValue}, 1, 255)`;
-        colors.push(color);
-    }
-    return colors;
+
+     fetchData() {
+       axios.get('http://localhost:8000/voting/')
+         .then(response => {
+           this.votings = response.data;
+           this.createChart();
+         })
+         .catch(error => {
+           console.error('Error fetching data:', error);
+         });
+     },
+     createChart() {
+       const ctx = this.$refs.myChart.getContext('2d');
+       new Chart(ctx, {
+         type: 'bar',
+         data: {
+           labels: this.votings.map(voting => voting.name),
+           datasets: [{
+             label: 'Total Votes',
+             data: this.votings.map(voting => voting.tally.length || 0),
+             backgroundColor: this.colors,
+             borderWidth: 1,
+           }],
+         },
+         options: {
+           scales: {
+             y: {
+               beginAtZero: true,
+             },
+           },
+         },
+       });
+     },
+     createDetailChart() {
+       this.$nextTick(() => {
+         if (this.detailChart) {
+           this.detailChart.destroy();
+         }
+         const detailCtx = this.$refs.detailChartCanvas.getContext('2d');
+         this.detailChart = new Chart(detailCtx, {
+           type: 'pie',
+           data: {
+             labels: [],
+             datasets: [{
+               label: 'Votos',
+               data: this.selectedVoting.tally.map(optionTally => [optionTally].length || 0),
+               backgroundColor: this.colors,
+               borderWidth: 1,
+             }],
+           },
+           options: {
+             scales: {
+               y: {
+                 beginAtZero: true,
+               },
+             },
+           },
+         });
+       });
+     },
+     showDetails(voting) {
+       this.selectedVoting = voting;
+       this.createDetailChart();
+     },
+   },
+ };
+ 
+ // Función para generar la escala de colores
+ function generateRandomColors(numColors) {
+  const colors = [];
+  for (let i = 0; i < numColors; i++) {
+    const randomColor = getRandomColor();
+    colors.push(randomColor);
+  }
+  return colors;
 }
-</script>
 
-<style scoped>
-/* Agrega estilos específicos del componente si es necesario */
-</style>
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+ </script>
+ 
+ <style scoped>
+ /* Agrega estilos específicos del componente si es necesario */
+ </style>
