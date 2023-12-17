a0da06fd7fad8e619d829f1fc53f597901ecdd05
[[IsmaelRuizJurado]] <[[ismruijur@alum.us.es]]>
2023-12-17 12:53:56 +0100

    Fix: Quitado botón duplicado #205

--- decide-frontend/src/components/Home.vue
+++ decide-frontend/src/components/Home.vue
@@ -86,11 +86,6 @@ export default {
           <p class="fecha">Inicio: {{ formatDate(voting.start_date) }} </p>
           <button class="btn" @click="$router.push('/booth/' + voting.id)">Votar</button>
           <button class="btn" @click="$router.push('/' + voting.id + '/stats')">Estadisticas</button>
-        </div>
-        <p class="fecha">Inicio: {{ formatDate(voting.start_date) }} </p>
-        <button class="btn" @click="$router.push('/booth/' + voting.id)">Votar</button>
-      </div>
-
     </div>
   </div>
   <p class="voting-p" v-if="logged">Votaciones próximas</p>
@@ -123,9 +118,6 @@ export default {
           <button class="btn" @click="$router.push('/visualizer/' + voting.id)">Resultados</button>
           <button class="btn" @click="$router.push('/' + voting.id + '/stats')">Estadisticas</button>
         </div>
-        <p class="fecha">Inicio: {{ formatDate(voting.start_date) }} </p>
-        <p class="fecha">Fin: {{ formatDate(voting.end_date) }} </p>
-        <button class="btn" @click="$router.push('/visualizer/' + voting.id)">Resultados</button>
       </div>
 
     </div>
