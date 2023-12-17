3c96f6c3e86b967d250dd184a5a9ac7aae83ba39
anglorcas <anglorcas@alum.us.es>
2023-12-17 21:34:10 +0100

    fix: loadtestadminvoting deleted #203

--- decide-frontend/src/router.js
+++ decide-frontend/src/router.js
@@ -17,6 +17,7 @@ import Register from './components/Register.vue';
 import EmailAuthentication from './components/EmailAuthentication.vue';
 import PageNotFound from './components/PageNotFound.vue';
 import UserData from './components/UserData.vue';
+import VotingStats from './components/VotingStats.vue';
 
 const routes = [
   { path: '/admin/voting', component: Voting },
@@ -37,6 +38,7 @@ const routes = [
   { path: '/login', component: UserLogin },
   { path: '/:id/stats', component: Stats },
   { path: '/:pathMatch(.*)*', component: PageNotFound },
+  { path: '/admin/voting/stats', component: VotingStats },
 ];
 
 const router = createRouter({
--- decide/decide/test_selenium.py
+++ decide/decide/test_selenium.py
@@ -120,10 +120,3 @@ class FrontendTest(StaticLiveServerTestCase):
         self.driver.get(f"http://localhost:{settings.FRONTEND_TEST_PORT}/asdfgasdf")
         message = self.driver.find_element(By.TAG_NAME, "h1").text
         self.assertEquals(message, "404")
-
-    def test_correctLoadVotingAdminStatsTest(self):
-        self.driver.get(f"http://localhost:{settings.FRONTEND_TEST_PORT}/admin/voting/stats")
-        self.driver.set_window_size(1064, 692)
-        h2_element = self.driver.find_element(By.CSS_SELECTOR, "h2")
-        #Comprueba que los elementos se cargaron correctamente
-        assert h2_element.text == "Datos de Votaciones"
